20ddbe05e395c9f7ce869656b3a5decca597e830
Matthew Dahl <dahl.matthew@gmail.com>
2016-12-30 12:58:29 -0800

    Add eslint-plugin-ember-standard and eslint-plugin-ocd

--- tests/index-spec.js
+++ tests/index-spec.js
@@ -19,6 +19,80 @@ describe('config', () => {
     expect(config.extends[0]).to.equal('standard')
   })
 
+  describe('ember-standard plugin', function () {
+    it('is included', function () {
+      expect(config.plugins).to.include('ember-standard')
+    })
+
+    it('includes computed-property-readonly rule', function () {
+      expect(config.rules['ember-standard/computed-property-readonly']).not.to.equal(undefined)
+    })
+
+    it('includes destructure rule', function () {
+      expect(config.rules['ember-standard/destructure']).not.to.equal(undefined)
+    })
+
+    it('includes import rule', function () {
+      expect(config.rules['ember-standard/import']).not.to.equal(undefined)
+    })
+
+    it('includes logger rule', function () {
+      expect(config.rules['ember-standard/logger']).not.to.equal(undefined)
+    })
+
+    it('includes no-set-in-computed-property rule', function () {
+      expect(config.rules['ember-standard/no-set-in-computed-property']).not.to.equal(undefined)
+    })
+
+    it('includes no-settimeout rule', function () {
+      expect(config.rules['ember-standard/no-settimeout']).not.to.equal(undefined)
+    })
+
+    it('includes prop-types rule', function () {
+      expect(config.rules['ember-standard/prop-types']).not.to.equal(undefined)
+    })
+
+    it('includes single-destructure rule', function () {
+      expect(config.rules['ember-standard/single-destructure']).not.to.equal(undefined)
+    })
+  })
+
+  describe('mocha plugin', function () {
+    it('is included', function () {
+      expect(config.plugins).to.include('mocha')
+    })
+
+    it('includes handle-done-callback rule', function () {
+      expect(config.rules['mocha/handle-done-callback']).not.to.equal(undefined)
+    })
+
+    it('includes no-exclusive-tests rule', function () {
+      expect(config.rules['mocha/no-exclusive-tests']).not.to.equal(undefined)
+    })
+
+    it('includes no-global-tests rule', function () {
+      expect(config.rules['mocha/no-global-tests']).not.to.equal(undefined)
+    })
+
+    it('includes no-pending-tests rule', function () {
+      expect(config.rules['mocha/no-pending-tests']).not.to.equal(undefined)
+    })
+
+    it('includes no-skipped-tests rule', function () {
+      expect(config.rules['mocha/no-skipped-tests']).not.to.equal(undefined)
+    })
+  })
+
+  describe('ocd plugin', function () {
+    it('is included', function () {
+      expect(config.plugins).to.include('ocd')
+    })
+
+    it('includes sort-imports rule', function () {
+      expect(config.rules['ocd/sort-imports']).not.to.equal(undefined)
+    })
+  })
+
   it('should include camelcase', () => {
     expect(config.rules.camelcase).not.to.equal(undefined)
   })
@@ -39,4 +113,3 @@ describe('config', () => {
     expect(config.rules['valid-jsdoc']).not.to.equal(undefined)
   })
 })
-
